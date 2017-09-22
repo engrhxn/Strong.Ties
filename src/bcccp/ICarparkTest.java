fa6dee8bc7954c1e9e31f91e44178257da1aed60
ashaik10 <30788158+ashaik10@users.noreply.github.com>
2017-09-23 02:16:32 +1000

    Icarpark test case 5

--- src/bcccp/ICarparkTest.java
+++ src/bcccp/ICarparkTest.java
@@ -1,26 +1,8 @@
 
-	@Test
-	public final void testDeregister() {
-		  System.out.println("Current test is " + getDeregister());
- // TODO
-	}
-
-	private String getDeregister() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-@Test
-	public final void testIsFull() {
-		 System.out.println("Current test is " + getIsfull()); // TODO
-	}
-
-	private String getIsfull() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-}
-
-
-
 	
+public class ICarparkTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
