655962bbfd07215e9a107482b36553c58ade89eb
ashaik10 <30788158+ashaik10@users.noreply.github.com>
2017-09-23 01:31:39 +1000

    Icarpark testcase 1

--- src/bcccp/ICarparkTest.java
+++ src/bcccp/ICarparkTest.java
@@ -7,14 +7,7 @@ import org.junit.Test;
 
 public class ICarparkTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
+	
 	@After
 	public void tearDown() throws Exception {
 	}
