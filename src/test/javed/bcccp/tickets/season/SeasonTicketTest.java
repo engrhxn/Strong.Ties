d26e0b54de7c203562886a58cf8f0bc80a414171
iota333 <m.javed_qureshi@yahoo.com>
2017-09-22 19:17:01 +1000

    SeasonTicket Class been update according to new code given in resources

--- src/test/javed/bcccp/tickets/season/SeasonTicketTest.java
+++ src/test/javed/bcccp/tickets/season/SeasonTicketTest.java
@@ -1,13 +1,105 @@
 package bcccp.tickets.season;
 
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class SeasonTicket implements ISeasonTicket {
 
-/**
- * Created by jaju on 9/18/17.
- */
-class SeasonTicketTest {
+	private List<IUsageRecord> usages;
+	private IUsageRecord currentUsage = null;
+
+	private String ticketId;
+	private String carparkId;
+	private long startValidPeriod;
+	private long endValidPeriod;
+
+	public SeasonTicket (String ticketId, String carparkId,
+						 long startValidPeriod,
+						 long endValidPeriod) {
+		this.ticketId = ticketId;
+		this.carparkId =carparkId;
+		this.startValidPeriod = startValidPeriod;
+		this.endValidPeriod = endValidPeriod;
+
+		usages = new ArrayList<IUsageRecord>();
+	}
 
 
-}
+	@Override
+	public String getId() {
+		return ticketId;
+	}
+
+
+	@Override
+	public String getCarparkId() {
+		return carparkId;
+	}
+
+
+	@Override
+	public long getStartValidPeriod() {
+		return startValidPeriod;
+	}
+
+
+	@Override
+	public long getEndValidPeriod() {
+		return endValidPeriod;
+	}
+
+
+	@Override
+	public boolean inUse() {
+		return currentUsage != null;
+	}
+
+
+	@Override
+	public void recordUsage(IUsageRecord record) {
+		currentUsage = record;
+		if (!usages.contains(record) ) {
+			usages.add(record);
+		}
+
+	}
+
+
+	@Override
+	public IUsageRecord getCurrentUsageRecord() {
+		return currentUsage;
+	}
+
+
+	@Override
+	public List<IUsageRecord> getUsageRecords() {
+		return Collections.unmodifiableList(usages);
+	}
+
+
+	@Override
+	public void endUsage(long dateTime) {
+		if (currentUsage == null) throw new RuntimeException("SeasonTicket.endUsage : ticket is not in use");
+
+		currentUsage.finalise(dateTime);
+		currentUsage = null;
+
+	}
+
+
+
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Carpark    : " + carparkId + "\n" +
+				"Ticket No  : " + ticketId + "\n" );
+		for (IUsageRecord usage : usages) {
+			builder.append(usage.toString() + "\n");
+		}
+		return builder.toString();
+	}
+
+
+} 
+
+ 	
