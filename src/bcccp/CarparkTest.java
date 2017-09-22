9099ac6f79d102bd11396bbc8b04b714d4f8e7c5
ashaik10 <30788158+ashaik10@users.noreply.github.com>
2017-09-23 00:26:40 +1000

    CarparkTest Test Case 1

--- src/bcccp/CarparkTest.java
+++ src/bcccp/CarparkTest.java
@@ -5,6 +5,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CarparkTest {
+@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
 
 
 }
