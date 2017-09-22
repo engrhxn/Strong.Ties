0e0a6a470b5bd2f0bd234d400b37f990e258bc65
mjaved06 <mjaved.jazz@gmail.com>
2017-09-22 21:47:35 +1000

    SeasonTicketDAOTest

---
+++ src/test/javed/bcccp/tickets/season/SeasonTicketDAOTest.java
@@ -0,0 +1,55 @@
+package bcccp.tickets.season;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by javed on 9/19/17.
+ */
+
+
+class SeasonTicketDAOTest {
+
+    @Test
+    void SeasonTicketDAO(){
+        //Creating Object
+        IUsageRecordFactory factory = new IUsageRecordFactory() {
+            @Override
+            public IUsageRecord make(String ticketId, long startDateTime) {
+                return null;
+            }
+        };
+
+     SeasonTicketDAO(factory);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
