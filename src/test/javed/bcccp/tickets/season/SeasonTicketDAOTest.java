c81f76ce87b7324eeca981f5adc9efa2dc9d0e59
mjaved06 <mjaved.jazz@gmail.com>
2017-09-22 22:25:40 +1000

    Integration Test 6

--- src/test/javed/bcccp/tickets/season/SeasonTicketDAOTest.java
+++ src/test/javed/bcccp/tickets/season/SeasonTicketDAOTest.java
@@ -1,5 +1,6 @@
 package bcccp.tickets.season;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -174,5 +175,23 @@ class SeasonTicketDAOTest {
         };
         SeasonTicketDAO seasonTicketDAO = new SeasonTicketDAO(factory);
         iSeasonTicket = seasonTicketDAO.findTicketById("736366336363");
+        assertEquals(0,seasonTicketDAO.getNumberOfTickets());
+    }
+
+    @Test
+    void recordTicketEntry(){
+        // Creating Object
+        ISeasonTicket iSeasonTicket;
+        IUsageRecordFactory factory = new IUsageRecordFactory() {
+            @Override
+            public IUsageRecord make(String ticketId, long startDateTime) {
+                return null;
+            }
+        };
+        SeasonTicketDAO seasonTicketDAO = new SeasonTicketDAO(factory);
+        iSeasonTicket = seasonTicketDAO.findTicketById("736366336363");
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            seasonTicketDAO.recordTicketEntry("1");
+        });
     }
 }
