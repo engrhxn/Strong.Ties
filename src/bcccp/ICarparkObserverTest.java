d6c272e98effb90035eaed5a1867125c61d6a6e4
ashaik10 <30788158+ashaik10@users.noreply.github.com>
2017-09-23 01:26:49 +1000

    Carparkobserver test case 2

--- src/bcccp/ICarparkObserverTest.java
+++ src/bcccp/ICarparkObserverTest.java
@@ -1,23 +1,6 @@
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-public class ICarparkObserverTest {
-
 	@Before
 	public void setUp() throws Exception {
 		
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testNotifyCarparkEvent() {
-		fail("Not yet implemented");
-	}
-
-}
+	
