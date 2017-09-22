5ed7e9abb1d0b64979258ab6295959dc95557709
mjaved06 <mjaved.jazz@gmail.com>
2017-09-22 22:31:07 +1000

    Integration Test 7

--- src/test/javed/bcccp/tickets/season/SeasonTicketDAOTest.java
+++ src/test/javed/bcccp/tickets/season/SeasonTicketDAOTest.java
@@ -194,4 +194,21 @@ class SeasonTicketDAOTest {
             seasonTicketDAO.recordTicketEntry("1");
         });
     }
+
+    @Test
+    void recordTicketExit(){
+        // Creating Object
+        ISeasonTicket iSeasonTicket;
+        IUsageRecordFactory factory = new IUsageRecordFactory() {
+            @Override
+            public IUsageRecord make(String ticketId, long startDateTime) {
+                return null;
+            }
+        };
+        SeasonTicketDAO seasonTicketDAO = new SeasonTicketDAO(factory);
+        iSeasonTicket = seasonTicketDAO.findTicketById("736366336363");
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            seasonTicketDAO.recordTicketExit("1"); //
+        });
+    }
 }
