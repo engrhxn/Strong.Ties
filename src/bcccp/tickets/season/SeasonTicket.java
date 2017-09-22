1083573f24156a2b2ca87a49ddd6c8a644f8f446
mjaved06 <m.javed_qureshi@yahoo.com>
2017-09-22 19:31:09 +1000

    SeasonTicket Class been update according to new code given in resources

--- src/bcccp/tickets/season/SeasonTicket.java
+++ src/bcccp/tickets/season/SeasonTicket.java
@@ -9,150 +9,99 @@ import java.util.Collections;
 import java.util.List;
 
 public class SeasonTicket implements ISeasonTicket {
-	
+
 	private List<IUsageRecord> usages;
 	private IUsageRecord currentUsage = null;
-	
+
 	private String ticketId;
 	private String carparkId;
 	private long startValidPeriod;
 	private long endValidPeriod;
-	
-	public SeasonTicket (String ticketId, 
-			             String carparkId, 
-			             long startValidPeriod,
-			             long endValidPeriod) {
-		this.ticketId = ticketId; 
- 		this.carparkId = carparkId; 
- 		this.startValidPeriod = startValidPeriod; 
- 		this.endValidPeriod = endValidPeriod; 
 
+	public SeasonTicket (String ticketId, String carparkId,
+						 long startValidPeriod,
+						 long endValidPeriod) {
+		this.ticketId = ticketId;
+		this.carparkId =carparkId;
+		this.startValidPeriod = startValidPeriod;
+		this.endValidPeriod = endValidPeriod;
+
+		usages = new ArrayList<IUsageRecord>();
 	}
 
-        //No Argument constructor  
- 	public SeasonTicket() { 
-	} 
-
-
-       //Set ticket Id  
- 	public void setTicketId(String ticketId) { 
- 		this.ticketId = ticketId; 
- 	} 
- 	
-       //Set caroark id 
- 	public void setCarparkId(String carparkId) { 
- 		this.carparkId = carparkId; 
- 	} 
- 	
-       //Set start valid period  
- 	public void setStartValidPeriod(long startValidPeriod) { 
- 		this.startValidPeriod = startValidPeriod; 
- 	} 
- 	
-       //Set end valid period  
- 	public void setEndValidPeriod(long endValidPeriod) { 
- 		this.endValidPeriod = endValidPeriod; 
-        } 
 
 	@Override
 	public String getId() {
-		this.ticketId = ticketId;
+		return ticketId;
 	}
+
 
 	@Override
 	public String getCarparkId() {
-		this.carparkId = carparkId;
+		return carparkId;
 	}
+
 
 	@Override
 	public long getStartValidPeriod() {
-		this.startValidPeriod = startValidPeriod
+		return startValidPeriod;
 	}
+
 
 	@Override
 	public long getEndValidPeriod() {
-		this.endValidPeriod = endValidPeriod;
+		return endValidPeriod;
 	}
+
 
 	@Override
 	public boolean inUse() {
-		
-		return false;
+		return currentUsage != null;
 	}
+
 
 	@Override
 	public void recordUsage(IUsageRecord record) {
-		File writeUsageFile = new File("../Anonymous/UsageRecordData.txt"); 
- 		if(writeUsageFile.exists()) { 
- 			try { 
- 				PrintWriter writeFile = new PrintWriter(writeUsageFile); 
- 				writeFile.print(record.getSeasonTicketId()); 
- 				writeFile.print("\t"); 
- 				writeFile.print(record.getStartTime()); 
- 				writeFile.print("\t"); 
- 				writeFile.print(record.getEndTime()); 
- 			} catch (FileNotFoundException e) { 
- 				e.printStackTrace(); 
- 			} 
- 		} 
+		currentUsage = record;
+		if (!usages.contains(record) ) {
+			usages.add(record);
+		}
 
-		
 	}
+
 
 	@Override
 	public IUsageRecord getCurrentUsageRecord() {
-		return null;
+		return currentUsage;
 	}
 
-	@Override
-	public void endUsage(long dateTime) {
-		try { 
- 			 
- 			Scanner readUsageRecord = new Scanner(new File("../Anonymous/UsageRecordData.txt")); 
- 			 
- 			while(readUsageRecord.hasNext()){ 
- 				String usageLine = readUsageRecord.nextLine(); 
- 				if(usageLine.contains(ticketId)){ 
- 					String[] usageRecArray = usageLine.split("\t"); 
- 					UsageRecord usageRec = new UsageRecord(); 
- 					usageRec.ticketId = usageRecArray[0]; 
- 					usageRec.startDateTime = Date.parse(usageRecArray[1]); 
- 					usageRec.endDateTime = dateTime; 
- 			} 
- 		  } 
- 				 
- 		} catch (FileNotFoundException e) { 
- 			e.printStackTrace(); 
- 		} 
-
-		
-	}
 
 	@Override
 	public List<IUsageRecord> getUsageRecords() {
-		List<IUsageRecord> usageRecordList = new ArrayList<>();
-		
-		try {
-			
-			Scanner readUsageRecord = new Scanner(new File("../Anonymous/UsageRecordData.txt"));
-			
-			while(readUsageRecord.hasNext()){
-				String usageLine = readUsageRecord.nextLine();
-				if(usageLine.contains(ticketId)){
-					String[] usageRecArray = usageLine.split("\t");
-					UsageRecord usageRec = new UsageRecord();
-					usageRec.ticketId = usageRecArray[0];
-					usageRec.startDateTime = Date.parse(usageRecArray[1]);
-					
-					usageRecordList.add(usageRec);
-				}
-			}
-				
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-			return usageRecordList;
+		return Collections.unmodifiableList(usages);
 	}
 
 
-}
+	@Override
+	public void endUsage(long dateTime) {
+		if (currentUsage == null) throw new RuntimeException("SeasonTicket.endUsage : ticket is not in use");
+
+		currentUsage.finalise(dateTime);
+		currentUsage = null;
+
+	}
+
+
+
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Carpark    : " + carparkId + "\n" +
+				"Ticket No  : " + ticketId + "\n" );
+		for (IUsageRecord usage : usages) {
+			builder.append(usage.toString() + "\n");
+		}
+		return builder.toString();
+	}
+
+
+} 
