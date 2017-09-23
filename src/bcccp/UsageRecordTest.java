b6bf0a0b166041a93778843cda65ca25168a7f7a
akshithreddy19 <31196150+akshithreddy19@users.noreply.github.com>
2017-09-23 17:06:29 +1000

    Usage Record Test Case 4

--- src/bcccp/UsageRecordTest.java
+++ src/bcccp/UsageRecordTest.java
@@ -26,3 +26,12 @@ class UsageRecordTest {
         //Using assertEqual method to check if the return value is good or not
         assertEquals(time, usageRecord.getStartTime());
     }
+@Test
+    void getEndTime(){
+        //Creating object
+        UsageRecord usageRecord = new UsageRecord("NobalPark1111", 1111);
+        //Creating time as long variable
+        long time = 0;
+        //Using assertEqual method to check if the return value is good or not
+        assertEquals(time, usageRecord.getEndTime());
+    }
