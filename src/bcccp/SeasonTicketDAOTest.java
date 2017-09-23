25e001713fc11a727dae4f416eb2c9ed93bda6c5
akshithreddy19 <31196150+akshithreddy19@users.noreply.github.com>
2017-09-23 16:38:05 +1000

    Season Ticket DAO Test case 6

--- src/bcccp/SeasonTicketDAOTest.java
+++ src/bcccp/SeasonTicketDAOTest.java
@@ -1,28 +1,4 @@
 
-
-    
-
-        
-
-    
-
-    
-
-    @Test
-    void getNumberOfTickets(){
-        // Creating Object
-        ISeasonTicket iSeasonTicket;
-        IUsageRecordFactory factory = new IUsageRecordFactory() {
-            @Override
-            public IUsageRecord make(String ticketId, long startDateTime) {
-                return null;
-            }
-        };
-        SeasonTicketDAO seasonTicketDAO = new SeasonTicketDAO(factory);
-        iSeasonTicket = seasonTicketDAO.findTicketById("736366336363");
-        assertEquals(0,seasonTicketDAO.getNumberOfTickets());
-    }
-
     @Test
     void recordTicketEntry(){
         // Creating Object
