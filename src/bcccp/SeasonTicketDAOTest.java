82dcf84ab63256966b1867c96640e44e886a9318
akshithreddy19 <31196150+akshithreddy19@users.noreply.github.com>
2017-09-23 16:01:23 +1000

    Season Ticket DAO Testcase 1

--- src/bcccp/SeasonTicketDAOTest.java
+++ src/bcccp/SeasonTicketDAOTest.java
@@ -1,18 +1,4 @@
-package bcccp.tickets.season;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- * Created by jaju on 9/19/17.
- */
-
-
-class SeasonTicketDAOTest {
 
     @Test
     void SeasonTicketDAO(){
